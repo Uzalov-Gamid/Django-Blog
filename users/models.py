921f12a611f6fe488ce26e82bdb7409fbeab1038
Uzalov-Gamid <uzalov.gamid@gmail.com>
2024-04-27 15:56:09 +0300

    feat: default photo

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    # Добавляем поле для фотографии профиля
+    avatar = models.ImageField(
+        upload_to='avatars/', default='avatars/default.png', blank=True, null=True)
+
+    class Meta:
+        managed = True
+
+
+# Исправляем конфликт имен для полей groups и user_permissions
+CustomUser._meta.get_field(
+    'groups').remote_field.related_name = 'customuser_set'
+CustomUser._meta.get_field(
+    'user_permissions').remote_field.related_name = 'customuser_set'
